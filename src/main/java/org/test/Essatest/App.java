01c010b9e11aa1e708163abe812c04ad6572d961
zengjin55 <3289714863@qq.com>
2018-04-13 09:41:56 +0800

    尝试搭建框架

---
+++ src/main/java/org/test/Essatest/App.java
@@ -0,0 +1,13 @@
+package org.test.Essatest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
